debfa63bc7d9f4b682819daceb3f32a55da1f9e5
ShawS <sunxycn98@gmail.com>
2023-04-11 17:41:41 -0400

    LED finished

--- src/main.cpp
+++ src/main.cpp
@@ -7,60 +7,59 @@
 #include <WiFi.h>
 
 
-/////     BME280 variables
+// BME280 variables
 #define SEALEVELPRESSURE_HPA (1026.4) // sea level pressure at ann arbor
 Adafruit_BME280 bme; // I2C
-unsigned long delayTime;
 
-/////     OLED display variables
+// OLED display variables
 #define SCREEN_WIDTH 128 // OLED display width, in pixels
 #define SCREEN_HEIGHT 64 // OLED display height, in pixels
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
 
-/////     button variabled
-#define BUTTON_PIN 36  // GIOP36 pin connected to button
-// Variables will change:
-int lastState = LOW;  // the previous state from the input pin
-int currentState;     // the current reading from the input pin
+// LED variables
+#define LED_PIN_R 2
+#define LED_PIN_G 15
 
-/////     helps declaration
+
+// helps declaration
 void printValues();
 void showBMEReadings(float temp, float humidity);
-void button();
+void LEDIndicator(float temp, float humidity);
 
 //////////////////////////////////////////////////
 /////     Main code
 //////////////////////////////////////////////////
 
 void setup() {
-  Serial.begin(115200);
-  bool bme_status;
-  // default settings
-  bme_status = bme.begin(0x76);  
-  if (!bme_status) {
+    Serial.begin(115200);
+    bool bme_status;
+    // default settings
+    bme_status = bme.begin(0x76);  
+    if (!bme_status) {
     Serial.println("Could not find a valid BME280 sensor, check wiring!");
     while (1);
-  }
+    }
 
-  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { // Address 0x3D for 128x64
+    if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { // Address 0x3D for 128x64
     Serial.println(F("SSD1306 allocation failed"));
     for(;;);
-  }
-  delay(2000);
-  display.clearDisplay();
-  display.setTextColor(WHITE);
+    }
+    delay(2000);
+    display.clearDisplay();
+    display.setTextColor(WHITE);
 
-  pinMode(BUTTON_PIN, INPUT_PULLUP);
+    pinMode(LED_PIN_R, OUTPUT);
+    pinMode(LED_PIN_G, OUTPUT);
+
 }
 
 void loop() { 
-  // printValues();
-  delay(2000);
-  float temp = bme.readTemperature();
-  float humidity = bme.readHumidity();
-  button();
-  showBMEReadings(temp, humidity);
-  delay(delayTime);
+    // printValues();
+    delay(2000);
+    float temp = bme.readTemperature();
+    float humidity = bme.readHumidity();
+    showBMEReadings(temp, humidity);
+    LEDIndicator(temp, humidity);
 }
 
 //////////////////////////////////////////////////
@@ -97,40 +96,40 @@ void showBMEReadings(float temp, float humidity){
     display.display(); 
 }
 
-void button(){
-  // read the state of the switch/button:
-  currentState = digitalRead(BUTTON_PIN);
-
-  if (lastState == HIGH && currentState == LOW) {
-    Serial.println("The button is pressed");
-  }
-  else if (lastState == LOW && currentState == HIGH){
-    Serial.println("The button is released");
-  }
-  lastState = currentState;
+void LEDIndicator(float temp, float humidity){
+    if (temp > 30 || humidity < 30){
+        digitalWrite(LED_PIN_R, HIGH); // on
+        delay(500);            
+        digitalWrite(LED_PIN_R, LOW);  // off
+        digitalWrite(LED_PIN_G, LOW);
+        delay(500);     
+    } else {
+        digitalWrite(LED_PIN_G, HIGH);
+        delay(500);  
+    }
 }
 
 void printValues() {
-  Serial.print("Temperature = ");
-  Serial.print(bme.readTemperature());
-  Serial.println(" *C");
-  
-  // Convert temperature to Fahrenheit
-  /*Serial.print("Temperature = ");
-  Serial.print(1.8 * bme.readTemperature() + 32);
-  Serial.println(" *F");*/
-  
-  Serial.print("Pressure = ");
-  Serial.print(bme.readPressure() / 100.0F);
-  Serial.println(" hPa");
+    Serial.print("Temperature = ");
+    Serial.print(bme.readTemperature());
+    Serial.println(" *C");
 
-  Serial.print("Approx. Altitude = ");
-  Serial.print(bme.readAltitude(SEALEVELPRESSURE_HPA));
-  Serial.println(" m");
+    // Convert temperature to Fahrenheit
+    /*Serial.print("Temperature = ");
+    Serial.print(1.8 * bme.readTemperature() + 32);
+    Serial.println(" *F");*/
 
-  Serial.print("Humidity = ");
-  Serial.print(bme.readHumidity());
-  Serial.println(" %");
+    Serial.print("Pressure = ");
+    Serial.print(bme.readPressure() / 100.0F);
+    Serial.println(" hPa");
 
-  Serial.println();
+    Serial.print("Approx. Altitude = ");
+    Serial.print(bme.readAltitude(SEALEVELPRESSURE_HPA));
+    Serial.println(" m");
+
+    Serial.print("Humidity = ");
+    Serial.print(bme.readHumidity());
+    Serial.println(" %");
+
+    Serial.println();
 }
